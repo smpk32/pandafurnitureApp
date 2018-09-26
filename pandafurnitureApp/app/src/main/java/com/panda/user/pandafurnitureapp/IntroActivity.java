3f77a174d2053863449c7dec740e7bd327555a38
smpk32 <43022845+smpk32@users.noreply.github.com>
2018-09-26 12:31:11 +0900

    메인액티비티까지 수정
    그만.....

--- pandafurnitureApp/app/src/main/java/com/panda/user/pandafurnitureapp/IntroActivity.java
+++ pandafurnitureApp/app/src/main/java/com/panda/user/pandafurnitureapp/IntroActivity.java
@@ -1,13 +1,21 @@
 package com.panda.user.pandafurnitureapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class IntroActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        context = this;
+
+
     }
 }
